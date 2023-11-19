716029436cf584252d9ba93ed277c2a445050a18
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-11-19 19:07:21 -0300

    styles header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,55 +1,73 @@
-import styles from './styles.module.scss'
-import Image from 'next/image';
-import logo from '../../../public/images/logo.jpeg'
-import { ActiveLink } from '../ActiveLink';
-import { FaYoutube, FaInstagram, FaWhatsapp, FaFacebook, FaTwitter, FaShoppingCart } from 'react-icons/fa'
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import logo from "../../../public/images/logohome1.jpeg";
+import { ActiveLink } from "../ActiveLink";
+import {
+  FaYoutube,
+  FaInstagram,
+  FaWhatsapp,
+  FaFacebook,
+  FaTwitter,
+  FaShoppingCart,
+} from "react-icons/fa";
 
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+       
+       
+        <div className={styles.logo}>
+          <ActiveLink legacyBehavior href="/" activeClassName={styles.active}>
+            <a>
+              <Image src={logo}
+               alt="Logo" 
+               width={210}
+                height={59} />
+            </a>
+          </ActiveLink>
+        </div>
 
-export function Header(){
-    return(
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
+        <nav>
+          <ActiveLink legacyBehavior href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
 
+          <ActiveLink
+            legacyBehavior
+            href="/posts"
+            activeClassName={styles.active}
+          >
+            <a>Dicas</a>
+          </ActiveLink>
 
-           <div className={styles.logo}>    
-            <ActiveLink legacyBehavior href="/" activeClassName={styles.active} >
-              
-           
-                <a>
-                    <Image src={logo} alt="Logo" />
-                </a> 
-                
-                
+          <ActiveLink
+            legacyBehavior
+            href="/ebooks"
+            activeClassName={styles.active}
+          >
+            <a>E-books</a>
+          </ActiveLink>
 
-                </ActiveLink>
-
-                </div> 
-
-                <nav>
-                    <ActiveLink legacyBehavior href="/" activeClassName={styles.active} >
-                        <a>Home</a>
-                    </ActiveLink>
-
-                    <ActiveLink legacyBehavior href="/posts" activeClassName={styles.active}>
-                        <a>Dicas</a>
-                    </ActiveLink>
-
-                    <ActiveLink legacyBehavior href="/ebooks" activeClassName={styles.active}>
-                        <a>E-books</a>
-                    </ActiveLink>
-                   
-                    <ActiveLink legacyBehavior href="/sobre" activeClassName={styles.active}>
-                        <a>Sobre mim</a>
-                    </ActiveLink>
-
-                   
-
-                </nav>
-                    <a className={styles.readyButton} type="button" href="https://hotmart.com/pt-br/marketplace/produtos/receitas-de-cafe-da-manha-praticas-e-saudaveis/H76773796C" target="_blank">  Produtos &nbsp;
-                    <FaShoppingCart size={25} /></a>
-                </div>
-        </header>
-        
-    )
-    
+          <ActiveLink
+            legacyBehavior
+            href="/sobre"
+            activeClassName={styles.active}
+          >
+            <a>Sobre mim</a>
+          </ActiveLink>
+        </nav>
+        <a
+          className={styles.readyButton}
+          type="button"
+          href="https://hotmart.com/pt-br/marketplace/produtos/receitas-de-cafe-da-manha-praticas-e-saudaveis/H76773796C"
+          target="_blank"
+        >
+          {" "}
+          Produtos &nbsp;
+          <FaShoppingCart size={25} />
+        </a>
+      </div>
+    </header>
+  );
 }
