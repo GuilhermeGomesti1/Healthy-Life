da50fb54f6d07b4fad6825226762f82445a9dbce
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-11-24 16:35:48 -0300

    styles slug ebooks

--- src/pages/ebooks/[slug].tsx
+++ src/pages/ebooks/[slug].tsx
@@ -7,6 +7,8 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { FaYoutube, FaInstagram, FaWhatsapp, FaFacebook, FaTwitter, FaShoppingCart } from 'react-icons/fa'
 import Prismic from '@prismicio/client';
+import ebookst from "../../../public/images/ebooks.png"
+import { useEffect } from 'react';
 interface EbookProps {
     ebook: {
         slug: string,
@@ -19,15 +21,47 @@ interface EbookProps {
 }
 
 export default function Ebook({ ebook }: EbookProps) {
+    useEffect(() => {
+
+        if (typeof window !== 'undefined') {
+          // O código abaixo será executado apenas no navegador
+          import('scrollreveal').then((ScrollRevealModule) => {
+            const ScrollReveal = ScrollRevealModule.default || ScrollRevealModule;
+    
+            const sr = ScrollReveal({
+              duration: 1000,
+              reset: false,
+              // Outras opções de configuração aqui
+            });
+    
+            sr.reveal('.animated-item', {
+              origin: 'bottom',
+              distance: '20px',
+              easing: 'ease-in-out',
+            });
+          });
+        }
+      }, []);
 
     return (
         <>
             <Head>
                 <title>{ebook.titlebook}</title>
             </Head>
+            <div>
+            <Image
+               className={`${styles.ebooks} animated-item`}
+                src={ebookst}
+                alt="foto home mobile"
+                width={490}
+                height={272}
+                loading="lazy"
+                placeholder="blur"
+              />
+            </div>
             <div className={styles.allpage}>
                 <main className={styles.container}>
-                    <article className={styles.post}>
+                    <article  className={`${styles.post} animated-item`}>  
                         <Image
                             quality={100}
                             src={ebook.coverbook}
@@ -37,12 +71,12 @@ export default function Ebook({ ebook }: EbookProps) {
                             placeholder="blur"
                             blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mPceQ4AAkYBiUQ8i/IAAAAASUVORK5CYII=" />
                        
-                        <h1>{ebook.titlebook}</h1>
-                        <time>{ebook.updatedAt}</time>
-                        <a className={styles.readyButton} type="button" href={ebook.linkebook} target="_blank">  Garanta já o seu! &nbsp;
+                        <h1 className='animated-item'>{ebook.titlebook} </h1> 
+                        <time className='animated-item'>{ebook.updatedAt}</time>
+                        <a className={`${styles.readyButton} animated-item`} type="button" href={ebook.linkebook} target="_blank">  Garanta já o seu! &nbsp;
                     <FaShoppingCart size={25} /></a>
                       
-                        <div className={styles.postContent} dangerouslySetInnerHTML={{ __html: ebook.descriptionbook }}></div>
+                        <div className={`${styles.postContent} animated-item`} dangerouslySetInnerHTML={{ __html: ebook.descriptionbook }}></div>
                        
                     </article>
                   
